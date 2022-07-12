cd617722a626ae020a6e8d0b2deb809f0bd1c9ba
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-07-12 15:56:27 +0200

    creazione struttura del database

--- database/migrations/2022_07_12_132912_create_trains_table.php
+++ database/migrations/2022_07_12_132912_create_trains_table.php
@@ -15,6 +15,17 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 30);
+            $table->string('departure_station', 30);
+            $table->string('arrive_station', 30);
+            $table->time('departure_hour', 4);
+            $table->time('arrive_hour', 4);
+            $table->integer('train_code');
+            $table->tinyInteger('carriages');
+            $table->boolean('in_time');
+            $table->boolean('cancelled');
+            $table->dateTime('current_date', 6);
+
             $table->timestamps();
         });
     }
